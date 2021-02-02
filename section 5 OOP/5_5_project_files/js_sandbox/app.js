36e3306f776b1c457a7d83120d72f8c92892163e
Eric Bourne <ericbourne6371@gmail.com>
2021-02-02 15:50:39 -0800

    finished sections 3 to 5 pt2

--- section 5 OOP/5_5_project_files/js_sandbox/app.js
+++ section 5 OOP/5_5_project_files/js_sandbox/app.js
@@ -1,25 +1,26 @@
 const personPrototypes = {
-  greeting: function() {
+  greeting: function () {
+    // greeting: set to a function
     return `Hello there ${this.firstName} ${this.lastName}`;
   },
-  getsMarried: function(newLastName) {
+  getsMarried: function (newLastName) {
     this.lastName = newLastName;
-  }
-}
-
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
+  },
+};
+//below uses alternative syntax
+const mary = Object.create(personPrototypes); //.create will take in prototypes
+mary.firstName = "Mary";
+mary.lastName = "Williams";
 mary.age = 30;
 
-mary.getsMarried('Thompson');
+mary.getsMarried("Thompson");
 
 console.log(mary.greeting());
 
 const brad = Object.create(personPrototypes, {
-  firstName: {value: 'Brad'},
-  lastName: {value: 'Traversy'},
-  age: {value: 36}
+  firstName: { value: "Brad" },
+  lastName: { value: "Traversy" },
+  age: { value: 36 },
 });
 
 console.log(brad);
