80273782ff1b83e1bbb5d41aa12dc3ab4d64cef2
Eric Bourne <ericbourne6371@gmail.com>
2021-02-08 16:42:47 -0800

    converting booklist into ES6 JS 3

--- section 5 OOP/6_5_BooklistES6/appEs6.js
+++ section 5 OOP/6_5_BooklistES6/appEs6.js
@@ -39,7 +39,7 @@ class UI {
     //set timeout
     setTimeout(function () {
       document.querySelector(".alert").remove();
-    }, 2050);
+    }, 3000);
 
     console.log("timeout works");
   }
@@ -53,9 +53,50 @@ class UI {
   clearFields() {
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
-    document.getElementById("isbs").vaue = "";
+    document.getElementById("isbn").value = "";
   }
 }
 
-//event listeners
-//event listeners for adding a book
+//
+//
+//
+//
+//
+// Event Listeners
+// Event Listeners for add book starting from form tag on html
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // get form values
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const isbn = document.getElementById("isbn").value;
+
+  // Instantiate book object
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Validate empty form fields
+  if (book.title === "" || book.author === "" || book.isbn === "") {
+    ui.showAlert("Please Fill Out Form fields, like C'Mon dawg", "error");
+  } else {
+    ui.addBookToList(book);
+    ui.showAlert("Book is added to the list Dawg", "success"); // message that shows in alert. className is the style class in HTML style which is red for error or green for success. The style class has to be within "" to make a string, or else it will pull a variable within the file
+    ui.clearFields();
+  }
+  //
+  console.log("Book added");
+  e.preventDefault();
+});
+// Event Listener for delete
+document.getElementById("book-list").addEventListener("submit", function (e) {
+  // Instantiate UI
+  const ui = new UI();
+  //delete book
+  ui.deleteBook(target);
+  // Show Alert
+  ui.showAlert("Book deleted from list", "success");
+  //
+  console.log("book deleted");
+  e.preventDefault();
+});
