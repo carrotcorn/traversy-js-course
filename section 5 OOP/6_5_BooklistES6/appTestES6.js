7ed23de21cca9b847d6407d4adb00e6fbc8ece49
Eric Bourne <ericbourne6371@gmail.com>
2021-02-10 12:44:13 -0800

    recoding ES6 practice script

--- section 5 OOP/6_5_BooklistES6/appTestES6.js
+++ section 5 OOP/6_5_BooklistES6/appTestES6.js
@@ -11,19 +11,19 @@ class Book {
 //what do i want to do in the book list
 //add book, delete book, show alerts with message, clear form fields
 class UI {
-  addToBookList(book) {
-     //create lists and rows via node creation 
-     const list = document.getElementById("book-list");
-     const row = document.createElement("tr");
-     //insert created rows
-     row.innerHTML = `
+  addBookToList(book) {
+    //create lists and rows via node creation
+    const list = document.getElementById("book-list");
+    const row = document.createElement("tr");
+    //insert created rows
+    row.innerHTML = `
      <td>${book.title}</td>
      <td>${book.author}</td>
      <td>${book.isbn}</td>
      <td><a href="#">Daaaaleete<a></td>
-     `
-     list.appendChild(row)
-     console.log('adding row function')
+     `;
+    list.appendChild(row);
+    console.log("adding row function");
   }
   showAlert(message, className) {}
   deleteBook(target) {}
@@ -35,18 +35,32 @@ class UI {
 //
 //Event Listeners
 //Add Book Event Listener
-document.getElementById("book-list").addEventListener("submit", function(event){
-//get form values
-const title = document.getElementById("title").value;
-const author = document.getElementById("author").value;
-const isbn = document.getElementById("isbn").value;
 
+document
+  .getElementById("book-list")
+  .addEventListener("submit", function (event) {
+    //get form values
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
 
+    //instantiate object, which will then create instances of the object
+    //meaning to create multiple objects
 
+    const book = new Book(title, author, isbn);
+    const ui = new UI();
 
+    //validation
+    if (title === "" || author === "" || isbn === "") {
+      ui.showAlert("Fill in the form", "error");
+      console.log("empty fields");
+    } else {
+      ui.addBookToList(book); //creates instance of object
+      ui.showAlert("Added Book!!!", "success");
+      console.log("successfully added book");
+    }
 
-
-event.preventDefault()
-})
+    event.preventDefault();
+  });
 //
 //Delete Handler Listener
