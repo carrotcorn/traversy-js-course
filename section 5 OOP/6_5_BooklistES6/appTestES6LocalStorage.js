74e6c23a15e50d0a3fdb524c4ef5f38ef6b8afa6
Eric Bourne <ericbourne6371@gmail.com>
2021-02-22 18:11:07 -0800

    adding local storage to book list ES6 project part 1

---
+++ section 5 OOP/6_5_BooklistES6/appTestES6LocalStorage.js
@@ -0,0 +1,115 @@
+class Book {
+  //properties of the object
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+//user interface functions
+//what do i want to do in the book list
+//add book, delete book, show alerts with message, clear form fields
+class UI {
+  addBookToList(book) {
+    //create lists and rows via node creation
+    const list = document.getElementById("book-list");
+    const row = document.createElement("tr");
+    //insert created rows
+    row.innerHTML = `
+     <td>${book.title}</td>
+     <td>${book.author}</td>
+     <td>${book.isbn}</td>
+     <td><a href="#" class="delete">Daaaaaaleeetee<a></td>
+     `;
+    list.appendChild(row);
+    console.log("adding row function");
+  }
+  showAlert(message, className) {
+    //show alert in text node element above
+    //below makes the alert only show once upon clicking submit
+    if (document.querySelector(`.alert.${className}`)) return;
+    //create html tag
+    const div = document.createElement("div");
+    //create class
+    div.className = `alert ${className}`; // in the if statement, the
+    // querySelector is referencing the alert thats being created here
+    //
+    //create text
+    div.appendChild(document.createTextNode(message));
+    //get parent node
+    const container = document.querySelector(".container"); //have to use querySelector() to be able to use "insertBefore()"
+    const form = document.querySelector("#book-form");
+    // with querySelector() need to use # or . notation for identifying either the ID or ClassName. I can use getElementById.
+
+    container.insertBefore(div, form);
+
+    // set timeOut
+    setTimeout(() => {
+      document.querySelector(".alert").remove();
+    }, 2000);
+  }
+  deleteBook(target) {
+    if (target.className === "delete") {
+      target.parentElement.parentElement.remove();
+      console.log("delete works");
+    }
+  }
+  clearFields() {
+    //clear text within the form
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("isbn").value = "";
+  }
+}
+//
+//
+//
+//
+//Event Listeners
+//Add Book Event Listener
+
+document
+  .getElementById("book-form")
+  .addEventListener("submit", function (event) {
+    //get form values
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const isbn = document.getElementById("isbn").value;
+
+    //instantiate object, which will then create instances of the object
+    //meaning to create multiple objects
+
+    const book = new Book(title, author, isbn);
+    const ui = new UI();
+    console.log("ui");
+
+    //validation
+    if (title === "" || author === "" || isbn === "") {
+      ui.showAlert("Fill in the form", "error");
+      console.log("empty fields");
+    } else {
+      ui.addBookToList(book); //creates instance of object
+      ui.showAlert("Added Book!!!", "success");
+      ui.clearFields();
+      console.log("successfully added book");
+    }
+
+    event.preventDefault();
+  });
+//
+//
+//
+//Delete Handler Listener
+document.getElementById("book-list").addEventListener("click", (event) => {
+  //using arrow function ^
+  //instantiate UI
+  const ui = new UI();
+  // call functions to be used within the event handler
+  ui.showAlert("Book Deleted", "success");
+  ui.deleteBook(event.target);
+
+  console.log("delete click works");
+
+  event.preventDefault();
+});
