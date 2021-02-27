aa69694d010b39c41a0292bc0de17036033f669a
Eric Bourne <ericbourne6371@gmail.com>
2021-02-27 15:41:24 -0800

    ASYNC/AJAX/Fetch

--- section 7 Async, Ajax, Fetch API/7_8_project_files/easyhttp/app.js
+++ section 7 Async, Ajax, Fetch API/7_8_project_files/easyhttp/app.js
@@ -1,10 +1,50 @@
-const http = new easyHTTP;
+const http = new easyHTTP();
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  if(err) {
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+// // get single post
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: "custom post",
+  body: "pooooooooost",
+};
+// // Create post
+// http.post("https://jsonplaceholder.typicode.com/posts", data, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// });
+
+// // Update post
+// http.put("https://jsonplaceholder.typicode.com/posts/5", data, (err, res) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// });
+
+// Delete post
+http.delete("https://jsonplaceholder.typicode.com/posts", data, (err, res) => {
+  if (err) {
     console.log(err);
   } else {
-    console.log(posts);
+    console.log(res);
   }
 });
--- section 7 Async, Ajax, Fetch API/7_8_project_files/easyhttp/easyhttp.js
+++ section 7 Async, Ajax, Fetch API/7_8_project_files/easyhttp/easyhttp.js
@@ -3,24 +3,55 @@ function easyHTTP() {
 }
 
 // Make an HTTP GET Request
-easyHTTP.prototype.get = function(url, callback) {
-  this.http.open('GET', url, true);
+easyHTTP.prototype.get = function (url, callback) {
+  this.http.open("GET", url, true);
 
-  let self = this;
-  this.http.onload = function() {
-    if(self.http.status === 200) {
+  let self = this; //object of easyHTTP
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      // self used instead of this to pertain within the onload function scope
       callback(null, self.http.responseText);
     } else {
-      callback('Error: ' + self.http.status);
+      callback("Error: " + self.http.status);
     }
-  }
+  };
 
   this.http.send();
-}
+};
 
 // Make an HTTP POST Request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open("POST", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  // dont need to use self due to use of arrow functions
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
-// Make an HTTP PUT Request
+// // Make an HTTP PUT Request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open("PUT", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  // dont need to use self due to use of arrow functions
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  };
+  this.http.send(JSON.stringify(data));
+};
 
 // Make an HTTP DELETE Request
-
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open("DELETE", url, true);
+  this.http.setRequestHeader("Content-type", "application/json");
+  // dont need to use self due to use of arrow functions
+  this.http.onload = () => {
+    if (this.http.status === 200) {
+      callback(null, this.http.responseText);
+    } else {
+      callback("Error: " + this.http.status);
+    }
+  };
+  this.http.send();
+};
