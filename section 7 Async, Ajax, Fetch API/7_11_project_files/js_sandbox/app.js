cfb7e4a1535fee63b369f5009cdfcf8b16ee53a7
Eric Bourne <ericbourne6371@gmail.com>
2021-02-28 14:33:05 -0800

    ASYNC/AJAX/Fetch

--- section 7 Async, Ajax, Fetch API/7_11_project_files/js_sandbox/app.js
+++ section 7 Async, Ajax, Fetch API/7_11_project_files/js_sandbox/app.js
@@ -1,60 +1,90 @@
-document.getElementById('button1').addEventListener('click', getText);
+document.getElementById("button1").addEventListener("click", getText);
 
-document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById("button2").addEventListener("click", getJson);
 
-document.getElementById('button3').addEventListener('click', getExternal);
-
+document.getElementById("button3").addEventListener("click", getExternal);
+// NO SEMI COLONS within promises
 // Get local text file data
+
 function getText() {
-  fetch('test.txt')
-    .then(function(res){
+  fetch("test.txt")
+    .then(function (res) {
       return res.text();
     })
-    .then(function(data) {
+    .then(function (data) {
       console.log(data);
-      document.getElementById('output').innerHTML = data;
+      document.getElementById("output").innerHTML = data;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
 }
-
 
 // Get local json data
 function getJson() {
-  fetch('posts.json')
-    .then(function(res){
+  fetch("posts.json", {
+    method: "GET",
+    // headers: {
+    //   "Content-Type": "application/json",
+    // },
+    // body: JSON.stringify({
+    //   data: "demo",
+    // }),
+  })
+    .then(function (res) {
       return res.json();
     })
-    .then(function(data) {
+    .then(handleErrors)
+    .then(function (data) {
       console.log(data);
-      let output = '';
-      data.forEach(function(post) {
+      let output = "";
+      data.forEach(function (post) {
         output += `<li>${post.title}</li>`;
       });
-      document.getElementById('output').innerHTML = output;
+      document.getElementById("output").innerHTML = output;
     })
-    .catch(function(err){
+    .catch(function (err) {
       console.log(err);
     });
 }
-
 
 // Get from external API
 function getExternal() {
-  fetch('https://api.github.com/users')
-    .then(function(res){
+  fetch("https://api.github.com/users")
+    .then((res) => {
       return res.json();
     })
-    .then(function(data) {
-      console.log(data);
-      let output = '';
-      data.forEach(function(user) {
-        output += `<li>${user.login}</li>`;
+    .then((data) => {
+      console.log("data");
+      let output = "";
+      data.forEach((user) => {
+        output += `<li>${user.repos_url}</li>`;
       });
-      document.getElementById('output').innerHTML = output;
-    })
-    .catch(function(err){
+      document.getElementById("output").innerHTML = output;
+    }) //cannot be any semi colons
+    .catch((err) => {
       console.log(err);
     });
 }
+function handleErrors(res) {
+  if (!res) throw new Error(res.error);
+  return res;
+}
+
+// function getExternal() {
+//   fetch('https://api.github.com/users')
+//     .then(function(res){
+//       return res.json();
+//     })
+//     .then(function(data) {
+//       console.log(data);
+//       let output = '';
+//       data.forEach(function(user) {
+//         output += `<li>${user.login}</li>`;
+//       });
+//       document.getElementById('output').innerHTML = output;
+//     })
+//     .catch(function(err){
+//       console.log(err);
+//     });
+// }
