213acae2509d8467ca7576085f37992491826568
Eric Bourne <ericbourne6371@gmail.com>
2021-02-28 15:19:44 -0800

    ASYNC/AJAX/Fetch

--- section 7 Async, Ajax, Fetch API/7_13_project_files/easyhttp2/app.js
+++ section 7 Async, Ajax, Fetch API/7_13_project_files/easyhttp2/app.js
@@ -1,9 +1,26 @@
-const http = new EasyHTTP;
+const http = new EasyHTTP();
 
-// Get Users
-// http.get('https://jsonplaceholder.typicode.com/users')
-//   .then(data => console.log(data))
-//   .catch(err => console.log(err));
+// Get Users asyncronously
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+// custom one below to show users in UI
+// http
+//   .get("https://jsonplaceholder.typicode.com/users")
+//   // .then((res) => {
+//   //   // console.log(res)
+//   //   return res.json()
+//   // })
+//   .then((data) => {
+//     let output = "";
+//     data.forEach((user) => {
+//       output += `<li>${user.name}</li>`;
+//     });
+//     document.getElementById("output").innerHTML = output;
+//   })
+//   .catch((err) => console.log(err));
 
 // User Data
 const data = {
@@ -12,17 +29,17 @@ const data = {
   email: 'jdoe@gmail.com'
 }
 
-// Create User
+// //Create user
 // http.post('https://jsonplaceholder.typicode.com/users', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
 
-// Update Post
+// // Update user
 // http.put('https://jsonplaceholder.typicode.com/users/2', data)
 //   .then(data => console.log(data))
 //   .catch(err => console.log(err));
 
-// Delete User
+// // Delete User
 http.delete('https://jsonplaceholder.typicode.com/users/2')
 .then(data => console.log(data))
 .catch(err => console.log(err));
--- section 7 Async, Ajax, Fetch API/7_13_project_files/easyhttp2/easyhttp2.js
+++ section 7 Async, Ajax, Fetch API/7_13_project_files/easyhttp2/easyhttp2.js
@@ -1,13 +1,11 @@
-
- class EasyHTTP {
-   
-  // Make an HTTP GET Request 
+class EasyHTTP {
+  // Make an HTTP GET Request
   get(url) {
     return new Promise((resolve, reject) => {
       fetch(url)
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
     });
   }
 
@@ -15,31 +13,31 @@
   post(url, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-type': 'application/json'
+          "Content-type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       })
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
     });
   }
 
-   // Make an HTTP PUT Request
-   put(url, data) {
+  // Make an HTTP PUT Request
+  put(url, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
-        method: 'PUT',
+        method: "PUT",
         headers: {
-          'Content-type': 'application/json'
+          "Content-type": "application/json",
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data),
       })
-      .then(res => res.json())
-      .then(data => resolve(data))
-      .catch(err => reject(err));
+        .then((res) => res.json())
+        .then((data) => resolve(data))
+        .catch((err) => reject(err));
     });
   }
 
@@ -47,17 +45,14 @@
   delete(url) {
     return new Promise((resolve, reject) => {
       fetch(url, {
-        method: 'DELETE',
+        method: "DELETE",
         headers: {
-          'Content-type': 'application/json'
-        }
+          "Content-type": "application/json",
+        }, //body is gone because im not sending any data
       })
-      .then(res => res.json())
-      .then(() => resolve('Resource Deleted...'))
-      .catch(err => reject(err));
+        .then((res) => res.json())
+        .then(() => resolve("Resource is now deleted..."))
+        .catch((err) => reject(err));
     });
   }
-
- }
-
- 
+}
