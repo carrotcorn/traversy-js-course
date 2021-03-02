91d610d8c8a293449bee2c5c2aa7f654cdda1661
Eric Bourne <ericbourne6371@gmail.com>
2021-03-02 13:15:24 -0800

    error handling practice

--- section 9 ErrorHandling and Regular Expressions/9_2_project_files/js_sandbox/app.js
+++ section 9 ErrorHandling and Regular Expressions/9_2_project_files/js_sandbox/app.js
@@ -4,13 +4,19 @@ re = /hello/i; // i =  case insensitive
 // re = /hello/g; // Global search
 
 // console.log(re);
+// // disregards the backslash in hello and logs just hello
 // console.log(re.source);
 
 // exec() - Return result in an array or null
+
+// array logs result at position 5 due to characters brad
+// const result = re.exec('brad hello world');
+
+// array logs result at position 0
 // const result = re.exec('hello world');
 
 // console.log(result);
-// console.log(result[0]);
+// console.log(result[0]);  
 // console.log(result.index);
 // console.log(result.input);
 
