d487b4c1d7759e7eee32b4dcd4d4aca765bb9f49
Eric Bourne <ericbourne6371@gmail.com>
2021-03-01 20:58:06 -0800

    added conditionals and math to convert api data into common data

--- section 8 API projects/8_6_weatherApp/weatherjs/ui.js
+++ section 8 API projects/8_6_weatherApp/weatherjs/ui.js
@@ -16,7 +16,35 @@ class UI {
       const celsius = Math.round(kelvin - 273.15).toFixed(1);
       return celsius;
     }
-
+    //getting wind direction in words
+    function windDirection() {
+      const degreeCompass = weather.wind.deg;
+      if (degreeCompass === 360 || degreeCompass === 0) {
+        return "North";
+      } else if (degreeCompass < 360 && degreeCompass > 270) {
+        return "North west";
+      } else if (degreeCompass === 270) {
+        return "West";
+      } else if (degreeCompass < 270 && degreeCompass > 180) {
+        return "South West";
+      } else if (degreeCompass === 180) {
+        return "South";
+      } else if (degreeCompass < 180 && degreeCompass > 90) {
+        return "South East";
+      } else if (degreeCompass === 90) {
+        return "East";
+      } else if (degreeCompass < 90 && degreeCompass > 0) {
+        return "North East";
+      } else {
+        return null;
+      }
+    }
+    //convert wind speed from m/sec to Km/h
+    function convertWindSpeed() {
+      const windSpeed = weather.wind.speed;
+      Math.round(windSpeed * 3.6).toFixed(1);
+      return windSpeed;
+    }
     this.location.textContent = weather.name;
     this.desc.textContent = weather.weather[0].description;
     //  this.string.textContent = weather.main.temp + "°Kelvin";
@@ -27,7 +55,10 @@ class UI {
       `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`
     );
     this.humidity.textContent = `Relative Humidity ${weather.main.humidity}%`;
-    this.dewpoint.textContent = `Wind Direction ${weather.wind.deg} degrees`;
-    this.wind.textContent = `Wind Speed ${weather.wind.speed} meters/second`;
+    this.dewpoint.textContent =
+      `Wind Direction: ${weather.wind.deg}° ` + windDirection();
+    //  this.dewpoint.textContent = `Wind Direction ${weather.wind.deg} degrees`;
+    this.wind.textContent = "Wind Speed: " + convertWindSpeed() + " Km/H";
+    //  this.wind.textContent = `Wind Speed ${weather.wind.speed} meters/second`;
   }
 }
