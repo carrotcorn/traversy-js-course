968e208219fe3472bea25a71d059b107d3ffc607
Eric Bourne <ericbourne6371@gmail.com>
2021-03-02 13:56:55 -0800

    error handling practice

--- section 9 ErrorHandling and Regular Expressions/9_4_project_files/js_sandbox/app.js
+++ section 9 ErrorHandling and Regular Expressions/9_4_project_files/js_sandbox/app.js
@@ -4,41 +4,42 @@ re = /hello/;
 re = /hello/i;
 
 // Metacharacter Symbols
-re = /^h/i;           // Must start with
-re = / world$/i;     // Must ends with
-re = /^hello$/i;     // Must begin and end with
-re = /h.llo/i;      // Matches any ONE character
-re = /h*llo/i;      // Matches any character 0 or more times
-re = /gre?a?y/i;    // Optional character
-re = /gre?a?y\?/i;    // Escape character 
-
+re = /^h/i; // Must start with
+re = / world$/i; // Must ends with
+re = /^hello$/i; // Must begin and end with
+re = /h.llo/i; // Matches any ONE character
+re = /h*llo/i; // Matches any character 0 or more times
+re = /gre?a?y/i; // Optional character
+re = /gre?a?y\?/i; // Escape character
 
 // Brackets [] - Character Sets
-re = /gr[ae]y/i;      // Must be an a or e
-re = /[GF]ray/i;      // Must be a G or F
-re = /[^GF]ray/i;      // Match anything except a G or F
-re = /[A-Z]ray/;      // Match any uppercase letter
-re = /[a-z]ray/;      // Match any lowercase letter
-re = /[A-Za-z]ray/;   // Match any  letter
-re = /[0-9][0-9]ray/;      // Match any digit
+re = /gr[ae]y/i; // Must be an a or e //better solution that the above ? optional characters
+re = /[GF]ray/i; // Must be a G or F
+re = /[^GF]ray/i; // Match anything except a G or F, Not G or F
+re = /[A-Z]ray/; // Match any uppercase letter
+re = /[a-z]ray/; // Match any lowercase letter
+re = /[A-Za-z]ray/; // Match any  letter
+re = /[0-9][0-9]ray/; // Match any digit
 
 // Braces {} - Quantifiers
-re = /Hel{2}o/i;      // Must occur exactly {m} amount of times
-re = /Hel{2,4}o/i;      // Must occur exactly {m} amount of times
-re = /Hel{2,}o/i;      // Must occur at least {m} times
+re = /Hel{2}o/i; // Must occur exactly {m} amount of times, checks for multiple 'l'
+re = /Hel{2,4}o/i; // Must occur exactly {m} amount of times, allows for 2 to 4 'l's
+re = /Hel{2,}o/i; // Must occur at least {m} times, at least 2 times
 
 // Paretheses () - Grouping
-re = /^([0-9]x){3}$/
+re = /([0-9]x){3}/; // checks number between 0-9 three times which matches below, but will also allow for more "3x"
+re = /^([0-9]x){3}$/;//this ends the string at only 3x3x3x
+
 
 // String to match
-const str = '3x3x3x';
+const str = "3x3x3x";
 
 // Log Results
 const result = re.exec(str);
 console.log(result);
 
 function reTest(re, str) {
-  if(re.test(str)) {
+  if (re.test(str)) {
     console.log(`${str} matches ${re.source}`);
   } else {
     console.log(`${str} does NOT match ${re.source}`);
