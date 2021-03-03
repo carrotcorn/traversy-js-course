c09cfa2b93377f524658279e0e4bef2233fbe746
Eric Bourne <ericbourne6371@gmail.com>
2021-03-02 18:18:24 -0800

    iterator project DONE

--- section 10 New ES6 features/10_1_project_files/js_sandbox/app.js
+++ section 10 New ES6 features/10_1_project_files/js_sandbox/app.js
@@ -21,14 +21,14 @@
 // console.log(names.next().value);
 // console.log(names.next().value);
 
-// Generator Example
+// // Generator Example // another example to loop thru the strings within the yield to return the values in order
 // function* sayNames() {
 //   yield 'Jack';
 //   yield 'Jill';
 //   yield 'John';
 // }
 
-// const name = sayNames();
+// const name = sayNames(); // calling this returns the object on line 8
 
 // console.log(name.next().value);
 // console.log(name.next().value);
@@ -46,6 +46,9 @@ function* createIds() {
 
 const gen = createIds();
 
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
 console.log(gen.next().value);
