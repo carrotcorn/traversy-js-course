95cf3a0438551e60cd5cfbb462c03821f3bb6c4d
Eric Bourne <ericbourne6371@gmail.com>
2021-03-07 18:14:50 -0800

    JS patterns Notes Singletons trouble

--- section 11 JS Patterns/11_3_project_files/js_sandbox/app.js
+++ section 11 JS Patterns/11_3_project_files/js_sandbox/app.js
@@ -1,19 +1,22 @@
-const Singleton = (function() {
+const Singleton = (function () {
   let instance;
 
   function createInstance() {
-    const object = new Object({name:'Eric'});
+    const object = new Object({ name: "Eric" });
     return object;
   }
 
   return {
-    getInstance: function() { //why is this function of getInstance?
-      if(!instance){
+    getInstance: function () {
+      //why is this function of getInstance?
+      // maybe because to get the instance of an object, the function has to be declared to be "of",
+      // therefore getInstance of the Object, the function name translates to "getInstance of function(){ return instance }" because I am returning an instance of the object hence the name being getInstance: function meaning getInstance of object using the function to return the instance variable
+      if (!instance) {
         instance = createInstance();
       }
       return instance;
-    }
-  }
+    },
+  };
 })();
 
 const instanceA = Singleton.getInstance();
