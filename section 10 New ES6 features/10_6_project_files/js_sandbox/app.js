21e9dffc79e01571dc834e4ddd626f71cf44ba4b
Eric Bourne <ericbourne6371@gmail.com>
2021-03-07 16:59:20 -0800

    Notes on Sets

--- section 10 New ES6 features/10_6_project_files/js_sandbox/app.js
+++ section 10 New ES6 features/10_6_project_files/js_sandbox/app.js
@@ -4,8 +4,8 @@ const set1 = new Set();
 
 // Add values to set
 set1.add(100);
-set1.add('A string');
-set1.add({name: 'John'});
+set1.add("A string");
+set1.add({ name: "John" });
 set1.add(true);
 set1.add(100);
 
@@ -20,7 +20,8 @@ set1.add(100);
 // Check for values
 // console.log(set1.has(100));
 // console.log(set1.has(50 + 50));
-// console.log(set1.has({name: 'John'}));
+// console.log(set1.has({name: 'John'})); this is a reference value that points to a different area in the memory.
+//this is pointing to the stack, not the heap. the stack is full of primitive data types, the heap is full of reference data to expand on the primitive data
 
 // Delete from set
 // set1.delete(100);
@@ -29,7 +30,7 @@ set1.add(100);
 
 // ITERATING THROUGH SETS
 
-// For..of 
+// For..of
 // for(let item of set1) {
 //   console.log(item);
 // }
