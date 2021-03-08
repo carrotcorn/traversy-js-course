1deb3801de6c56b14e2debde34a848ff96886430
Eric Bourne <ericbourne6371@gmail.com>
2021-03-08 15:31:19 -0800

    calorie counter project

--- section 12 TracalorieProject/12_2_project_files/tracalorie/app.js
+++ section 12 TracalorieProject/12_2_project_files/tracalorie/app.js
@@ -1,10 +1,43 @@
 //Storage Controller
 
 //Item Controller
-const ItemCtrl = (function(){
-   console.log('item controller');
-})
+const ItemCtrl = (function () {
+  //item constructor
+  const Item = function (id, name, calories) {
+    this.id = id;
+    this.name = name;
+    this.calories = calories;
+  };
+  // Data Structure / State
+  const data = {
+    items: [
+      {
+        id: 0,
+        name: "Steak Dinner",
+        calories: 1200,
+      },
+      {
+        id: 1,
+        name: "Pizza",
+        calories: 2000,
+      },
+      {
+        id: 2,
+        name: "Eggs Benedict",
+        calories: 3000,
+      },
+    ],
+    currentItems: null, //when update icon selected, this item will become the current item which will be put into the form to be updated
+    totalCalories: 0,
+  };
+  return {
+    logData: function () {
+      return data;
+    },
+  };
+})();
 
 //UI controller
-
+const UICtrl = (function () {})();
 //App controller
+const App = (function (ItemCtrl, UICtrl) {})(ItemCtrl, UICtrl);
--- section 12 TracalorieProject/12_3_project_files/tracalorie/app.js
+++ section 12 TracalorieProject/12_3_project_files/tracalorie/app.js
@@ -1,57 +1,47 @@
 // Storage Controller
 
-
 // Item Controller
-const ItemCtrl = (function(){
+const ItemCtrl = (function () {
   // Item Constructor
-  const Item = function(id, name, calories){
+  const Item = function (id, name, calories) {
     this.id = id;
     this.name = name;
     this.calories = calories;
-  }
+  };
 
   // Data Structure / State
   const data = {
     items: [
-      {id: 0, name: 'Steak Dinner', calories: 1200},
-      {id: 1, name: 'Cookie', calories: 400},
-      {id: 2, name: 'Eggs', calories: 300}
+      { id: 0, name: "Steak Dinner", calories: 1200 },
+      { id: 1, name: "Cookie", calories: 400 },
+      { id: 2, name: "Eggs", calories: 300 },
     ],
     currentItem: null,
-    totalCalories: 0
-  }
+    totalCalories: 0,
+  };
 
   // Public methods
   return {
-    logData: function(){
+    logData: function () {
       return data;
-    }
-  }
+    },
+  };
 })();
-
-
 
 // UI Controller
-const UICtrl = (function(){
-  
+const UICtrl = (function () {
   // Public methods
-  return {
-
-  }
+  return {};
 })();
 
-
-
 // App Controller
-const App = (function(ItemCtrl, UICtrl){
-
+const App = (function (ItemCtrl, UICtrl) {
   // Public methods
   return {
-    init: function(){
-      console.log('Initializing App...');
-    }
-  }
-  
+    init: function () {
+      console.log("Initializing App...");
+    },
+  };
 })(ItemCtrl, UICtrl);
 
 // Initialize App
